5f5dcb8dc347dd45f1e2bb2ebe7f64e4d1a73df0
handsomegyr <handsomegyr@126.com>
2017-08-17 22:46:25 +0800

    init

---
+++ Worker.cs
@@ -0,0 +1,48 @@
+﻿#region Using directives
+
+using System.Collections.Generic;
+using System.IO;
+
+#endregion
+
+namespace CopyFilesInOneFolder
+{
+    public class Worker
+    {
+
+        public static bool merge(string selectedPath, List<FileEntry> excelFileList, System.ComponentModel.BackgroundWorker backgroundWorker)
+        {
+
+            string dir = selectedPath + "mergeAll";
+            if(Directory.Exists(dir)){
+                Directory.Delete(dir,true);
+            }
+            DirectoryInfo di = Directory.CreateDirectory(dir);
+            
+            int i = 0;
+            foreach (var f in excelFileList)
+            {
+                i++;
+                if (backgroundWorker.CancellationPending)
+                {
+                    // Return without doing any more work.
+                    return false;
+                }
+
+                if (backgroundWorker.WorkerReportsProgress)
+                {
+                    backgroundWorker.ReportProgress(i);
+                }
+
+                if (!f.IsSelected) continue;
+                f.Status = "处理中";
+                File.Copy(f.Path, di.FullName + "/" + f.Name, true);
+                f.Status = "处理完成";
+            }
+            return true;
+
+        }
+
+        
+    }
+}
